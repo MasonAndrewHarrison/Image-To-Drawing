370be8fe2c170ca3c6d3f9adfe3969bd6d5ed65f
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2026-02-10 22:37:36 -0500

    commit

---
+++ canny_filter.py
@@ -0,0 +1,107 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+def convert(img, device="cpu"):
+
+    tensor = img
+    inverted = tensor.mean(0).unsqueeze(0).unsqueeze(0).to(device) / 256.0
+
+    gaussian_blur = torch.tensor([
+        [1, 2, 1],
+        [2, 4, 2],
+        [1, 2, 1],
+    ], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
+
+    big_blur = torch.tensor([
+        [1, 4, 6, 4, 1],
+        [4,16,24,16, 4],
+        [6,24,36,24, 6],
+        [4,16,24,16, 4],
+        [1, 4, 6, 4, 1],
+    ], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
+
+    sobel_x = torch.tensor([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1],
+    ], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
+
+    sobel_y = torch.tensor([
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1],
+    ], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
+
+    blurred = F.conv2d(inverted, gaussian_blur, padding=1)
+    sharp_x = F.conv2d(blurred, sobel_x, padding=1, groups=1) 
+    sharp_y = F.conv2d(blurred, sobel_y, padding=1, groups=1)
+
+    magnitude = torch.sqrt(sharp_x * sharp_x + sharp_y * sharp_y)
+    magnitude = magnitude / magnitude.max()
+
+    # θ = arctan(y/x) * 180 / π
+    angle = torch.atan2(sharp_y, sharp_x) * 180.0 / np.pi
+    angle = angle % 180
+
+    _, _, H, W = angle.shape
+
+    def angle_rounder(theda):
+        if theda >= torch.tensor([0], device=device) and theda <= torch.tensor([22.5], device=device):
+            return 0
+        elif theda > torch.tensor([22.5], device=device) and theda <= torch.tensor([67.5], device=device):
+            return 45
+        elif theda > torch.tensor([67.5], device=device) and theda <= torch.tensor([112.5], device=device):
+            return 90
+        elif theda > torch.tensor([112.5], device=device) and theda <= torch.tensor([157.5], device=device):
+            return 135
+        elif theda > torch.tensor([157.5], device=device) and theda <= torch.tensor([180], device=device):
+            return 0
+        else:
+            return -1
+
+    img = torch.zeros_like(magnitude)
+
+    # Non-Maximum Suppression
+    for x in range(1, W-1):
+        for y in range(1, H-1):
+
+            if angle_rounder(angle[:, :, y, x]) == 0:
+                if magnitude[:, :, y, x] > magnitude[:, :, y, x-1] and magnitude[:, :, y, x] > magnitude[:, :, y, x+1]:
+                    img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
+
+            elif angle_rounder(angle[:, :, y, x]) == 45:
+                if magnitude[:, :, y, x] > magnitude[:, :, y-1, x+1] and magnitude[:, :, y, x] > magnitude[:, :, y+1, x-1]:
+                    img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
+            
+            elif angle_rounder(angle[:, :, y, x]) == 90:
+                if magnitude[:, :, y, x] > magnitude[:, :, y-1, x] and magnitude[:, :, y, x] > magnitude[:, :, y+1, x]:
+                    img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
+
+            elif angle_rounder(angle[:, :, y, x]) == 135:
+                if magnitude[:, :, y, x] > magnitude[:, :, y-1, x-1] and magnitude[:, :, y, x] > magnitude[:, :, y+1, x+1]:
+                    img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
+
+    '''
+    # Hysteresis
+    for x in range(1, W-1):
+        for y in range(1, H-1):
+            
+            if img[:, :, y, x] is not torch.tensor([0], device=device):
+                top_left = img[:, :, y+1, x-1]
+                left = img[:, :, y, x-1]
+                bottom_left = img[:, :, y-1, x-1]
+                
+                top_right = img[:, :, y+1, x+1]
+                right = img[:, :, y, x+1]
+                bottom_right = img[:, :, y-1, x+1]
+
+                bottom = img[:, :, y-1, x]
+                top = img[:, :, y+1, x]
+
+                surrounding_pixels = torch.stack([top_left, left, bottom_left, top_right, right, bottom_right, bottom, top])
+
+                if (surrounding_pixels >= 0.5).any():
+                    img[:, :, y, x] = torch.tensor([1], device=device)'''
+
+    return img
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 import numpy as np
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from torchvision.datasets import ImageFolder
 from torchvision import datasets, transforms
+from canny_filter import convert
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = "cpu"
@@ -12,90 +12,14 @@ device = "cpu"
 transforms = transforms.Compose([transforms.ToTensor()])
 dataset = ImageFolder(root='img/', transform=transforms)
 
-tensor,_ = dataset[1]
-inverted = tensor.mean(0).unsqueeze(0).unsqueeze(0).to(device) / 256.0
+tensor,_ = dataset[0]
+tensor = tensor.requires_grad_(True)
 
-gaussian_blur = torch.tensor([
-    [1, 2, 1],
-    [2, 4, 2],
-    [1, 2, 1],
-], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
+print(tensor.requires_grad)
 
-big_blur = torch.tensor([
-    [1, 4, 6, 4, 1],
-    [4,16,24,16, 4],
-    [6,24,36,24, 6],
-    [4,16,24,16, 4],
-    [1, 4, 6, 4, 1],
-], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
+img = convert(tensor, device=device)
 
-sobel_x = torch.tensor([
-    [-1, 0, 1],
-    [-2, 0, 2],
-    [-1, 0, 1],
-], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
-
-sobel_y = torch.tensor([
-    [-1, -2, -1],
-    [0, 0, 0],
-    [1, 2, 1],
-], dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
-
-blurred = F.conv2d(inverted, gaussian_blur, padding=1)
-sharp_x = F.conv2d(blurred, sobel_x, padding=1, groups=1) 
-sharp_y = F.conv2d(blurred, sobel_y, padding=1, groups=1)
-
-magnitude = torch.sqrt(sharp_x * sharp_x + sharp_y * sharp_y)
-magnitude = magnitude / magnitude.max()
-
-# θ = arctan(y/x) * 180 / π
-angle = torch.atan2(sharp_y, sharp_x) * 180.0 / np.pi
-angle = angle % 180
-
-_, _, H, W = angle.shape
-
-def angle_rounder(theda):
-    if theda >= torch.tensor([0], device=device) and theda <= torch.tensor([22.5], device=device):
-        return 0
-    elif theda > torch.tensor([22.5], device=device) and theda <= torch.tensor([67.5], device=device):
-        return 45
-    elif theda > torch.tensor([67.5], device=device) and theda <= torch.tensor([112.5], device=device):
-        return 90
-    elif theda > torch.tensor([112.5], device=device) and theda <= torch.tensor([157.5], device=device):
-        return 135
-    elif theda > torch.tensor([157.5], device=device) and theda <= torch.tensor([180], device=device):
-        return 0
-    else:
-        return -1
-
-img = torch.zeros_like(magnitude)
-
-for x in range(1, W-1):
-    for y in range(1, H-1):
-
-        if angle_rounder(angle[:, :, y, x]) == 0:
-            if magnitude[:, :, y, x] > magnitude[:, :, y, x-1] and magnitude[:, :, y, x] > magnitude[:, :, y, x+1]:
-                img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
-
-        elif angle_rounder(angle[:, :, y, x]) == 45:
-            if magnitude[:, :, y, x] > magnitude[:, :, y-1, x+1] and magnitude[:, :, y, x] > magnitude[:, :, y+1, x-1]:
-                img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
-        
-        elif angle_rounder(angle[:, :, y, x]) == 90:
-            if magnitude[:, :, y, x] > magnitude[:, :, y-1, x] and magnitude[:, :, y, x] > magnitude[:, :, y+1, x]:
-                img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
-
-        elif angle_rounder(angle[:, :, y, x]) == 135:
-            if magnitude[:, :, y, x] > magnitude[:, :, y-1, x-1] and magnitude[:, :, y, x] > magnitude[:, :, y+1, x+1]:
-                img[:, :, y, x] = magnitude[:, :, y, x] if (magnitude[:, :, y, x] >= 0.05) else 0
-
-'''
-for x in range(1, W-1):
-    for y in range(1, H-1):
-        
-        if img[:, :, y, x] >= torch.tensor([0.05], device=device):'''
-
-
+print(img.grad)
 
 tensor = img
 
