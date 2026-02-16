8704f9df49d8818b06a04b3078cf2d4223ad09c6
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2026-02-16 12:50:28 -0500

    commit

--- main.py
+++ main.py
@@ -11,36 +11,49 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 transforms = transforms.Compose([transforms.ToTensor()])
 dataset = ImageFolder(root='img/', transform=transforms)
 
-tensor,_ = dataset[0]
-
-tensor = tensor.requires_grad_(True)
-tensor = tensor.mean(0).unsqueeze(0).unsqueeze(0).to(device) / 256.0
-
-tensor = filter.scale(tensor, 5)
-image = tensor.clone().detach()
-
-tensor = filter.variable_gaussian_blur(tensor, size=3, sigma=1.0)
-xdog = filter.ex_difference_of_gaussians(tensor)
-canny = filter.canny(tensor)
+fig, axes = plt.subplots(3, len(dataset), figsize=(16, 4))
 
 
+for i in range(len(dataset)):
 
-canny_display = canny.squeeze(0).squeeze(0).detach().cpu()
-xdog_display = xdog.squeeze(0).squeeze(0).detach().cpu()
-image = image.squeeze(0).squeeze(0).detach().cpu()
+    idx = 0
+    tensor,_ = dataset[i]
 
-fig, axes = plt.subplots(1, 3, figsize=(16, 4))
+    tensor = tensor.requires_grad_(True)
+    tensor = tensor.mean(0).unsqueeze(0).unsqueeze(0).to(device) / 256.0
 
-axes[0].imshow(canny_display, cmap='gray')
-axes[0].set_title('Canny')
-axes[0].axis('off')
+    image = tensor.clone().detach()
 
-axes[1].imshow(xdog_display, cmap='gray')
-axes[1].set_title('XDoG')
-axes[1].axis('off')
+    tensor = filter.variable_gaussian_blur(tensor, size=3, sigma=1.0)
+    
+    canny = filter.canny(tensor)
 
-axes[2].imshow(image, cmap='gray')
-axes[2].axis('off')
+    tensor = filter.scale(tensor, 5)
+
+    xdog = filter.ex_difference_of_gaussians(tensor)
+
+    canny_display = canny.squeeze(0).squeeze(0).detach().cpu()
+    xdog_display = xdog.squeeze(0).squeeze(0).detach().cpu()
+    image = image.squeeze(0).squeeze(0).detach().cpu()
+
+    
+
+    axes[idx, i].imshow(canny_display, cmap='gray')
+    axes[idx, i].set_title('Canny')
+    axes[idx, i].axis('off')
+
+    idx += 1
+
+    axes[idx, i].imshow(xdog_display, cmap='gray')
+    axes[idx, i].set_title('XDoG')
+    axes[idx, i].axis('off')
+
+    idx += 1
+
+    axes[idx, i].imshow(image, cmap='gray')
+    axes[idx, i].axis('off')
+
+    idx += 1
 
 
 
